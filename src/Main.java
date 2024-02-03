f867dc69fe110df5c20a62743d88327980701da9
Owin Abeysinghe <owinabeysinghe@gmail.com>
2024-02-03 17:52:09 -0500

    Changed a comment.

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         double interestAfterTwoMonths = 0; // Initialize interest after two months double variable
         interestAfterOneMonth = creditCardBalance * (0.17); // Set the interest after one month variable to 17% of the credit card balance
         interestAfterTwoMonths = (creditCardBalance + interestAfterOneMonth) * (0.17); // Set the interest after two months variable to 17% of the one-month interest added to the credit card balance
-        System.out.println("The credit card balance is " + creditCardBalance); // Output the credit card balance
+        System.out.println("The credit card balance is " + creditCardBalance); // Output the credit card initial balance
         System.out.println("The interest after one month is " + interestAfterOneMonth); // Output the interest after one month
         System.out.println("The interest after two months is " + interestAfterTwoMonths); // Output the interest after two months
     }
